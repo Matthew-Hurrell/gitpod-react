de16fe5bc1ceb702dc9e26635e6beeb75dd8eb12
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-03-20 21:02:48 +0000

    Initial commit

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Regular Function
+// function FunctionalGreeting() {
+//     return <h1>Hello from React!</h1>
+// }
+
+// Arrow Function
+const FunctionalGreeting = () => <h1>Hello from React!</h1>
+
+export default FunctionalGreeting;
